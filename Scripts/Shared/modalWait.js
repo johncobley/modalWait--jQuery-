56950a0dd9780d49427b37a232f8d1e6149b8055
johncobley <john.cobley@comtek.co.uk>
2015-05-28 17:54:39 +0100

    Updated
    Now has a semi-transparent layer between the wait dialog and the rest of
    the elements.
    Text can be added below the GIF.

--- Scripts/Shared/modalWait.js
+++ Scripts/Shared/modalWait.js
@@ -4,33 +4,62 @@
             return this.each(function () {
                 var settings = $.extend({
                     "image": "/Content/Images/ajax-loader_bar-transp-back.gif",
-                    "autoHide": true
+                    "autoHide": true,
+                    "modalTextPosition": "bottom"
                 }, options);
                 var $this = $(this);
                 var data = $this.data("modalWait");
                 if (!data) {
                     $this.wrap('<div class="modalWaitContainer" />');
                     $this.append('<img alt="wait" class="waitImg" />');
+                    if (settings.modalText)
+                        $this.append('<span class="modalWaitText" ></span>');
                     $this.after('<div class="modal" />');
+                    var modalWidth = parseInt($(".modal").css("width")),
+                    modalHeight = parseInt($(".modal").css("height")),
+                    htmlWidth = parseInt($("html").css("width")),
+                    htmlHeight = parseInt($("html").css("height"));
+                    if (htmlWidth > modalWidth)
+                        $(".modal").width($("html").outerWidth());
+                    if (htmlHeight > modalHeight)
+                        $(".modal").height($("html").outerHeight());
                     $this.addClass("modalWait");
                     if (settings.autoHide)
                         methods.hide.call($this, settings);
                     methods.option.call($this, settings);
                     $this.data("modalWait", true);
+                    $(".modalWait").windowCentred();
+                    $(window).resize(function () {
+                        $(".modalWait").windowCentred();
+                        var modalWidth = parseInt($(".modal").css("width"));
+                        var modalHeight = parseInt($(".modal").css("height"));
+                        var htmlWidth = parseInt($("html").css("width"));
+                        var htmlHeight = parseInt($("html").css("height"));
+                        if (htmlWidth > modalWidth)
+                            $(".modal").css("width", $("html").css("width"));
+                        if (htmlHeight > modalHeight)
+                            $(".modal").css("height", $("html").css("height"));
+                        $this.addClass("modalWait");
+                    });
+                    $(window).scroll(function () {
+                        $(".modal").windowCentred();
+                    });
                 }
             });
         },
-        option: function (settings) {
+        option: function (settings, val) {
+            if (typeof settings !== 'object')
+                eval("settings = { " + settings + ": \"" + val + "\" };");
             return this.each(function () {
                 var $this = $(this);
                 var removePx = /[0-9]*/g;
                 if (settings.height) {
-                    $this.css("height", settings.height);
+                    $this.height(settings.height);
                     var marginTop = parseFloat(settings.height.match(removePx)) / 2;
                     $this.css("margin-top", -marginTop);
                 }
                 if (settings.width) {
-                    $this.css("width", settings.width);
+                    $thiswidth(settings.width);
                     var marginLeft = parseFloat(settings.width.match(removePx)) / 2;
                     $this.css("margin-left", -marginLeft);
                 }
@@ -57,13 +86,34 @@
                 if (settings.image) {
                     $this.children("img").attr("src", settings.image);
                 }
+                if (settings.modalText) {
+                    $this.children(".modalWaitText").text(settings.modalText);
+                }
+                if (settings.modalText && settings.modalTextPosition) {
+                    if (settings.modalTextPosition == "top") {
+                        $this.children(".modalWaitText").css("top", 12);
+                    }
+                    if (settings.modalTextPosition == "bottom") {
+                        $this.children(".modalWaitText").css("top", 42);
+                    }
+                }
             });
         },
         show: function () {
-            return this.each(function() {
+            return this.each(function () {
+                var modalWidth = parseInt($(".modal").css("width")),
+                    modalHeight = parseInt($(".modal").css("height")),
+                    windowWidth = parseInt($(window).width()),
+                    windowHeight = parseInt($(window).height());
+                if (windowWidth > modalWidth)
+                    $(".modal").width($(window).width());
+                if (windowHeight > modalHeight)
+                    $(".modal").height($(window).height());
                 $(this).parents(".modalWaitContainer").show();
+                //$(".modal").windowCentred();
+                $(".modalWait").windowCentred();
             });
-            },
+        },
         hide: function () {
             return this.each(function () {
                 $(this).parents(".modalWaitContainer").hide();
